e64cfaac931e563988add4296beff81fe0800725
Misbahul-Alam <misbahulalm78@gmail.com>
2024-04-30 20:22:09 +0600

    Update Coding

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className=" p-4 bg-white rounded w-full max-w-[450px]">
+      <h2 className="font-medium text-xl">Create an account</h2>
+
+    </div>
+  );
+}
